daef608aca86b6dd651af804f2fe97e5332b355b
y-shonn09 <nukomaru9@gmail.com>
2022-03-31 22:59:43 +0900

    first

---
+++ script.js
@@ -0,0 +1,21 @@
+jQuery(function($){
+	// var current;
+	$.scrollify({
+		section:'.box',
+		setHeights:false,
+        overflowScroll:true,
+        updateHash: false,
+        
+	});
+});
+
+$('a[href^="#"]').click(function() {
+    let speed = 400;
+    let href = $(this).attr("href");
+    let target = $(href == "#" || href == "" ? 'html' : href);
+    let position = target.offset().top;
+    $('body,html').animate({
+      scrollTop: position
+    }, speed, 'swing');
+    return false;
+  });
